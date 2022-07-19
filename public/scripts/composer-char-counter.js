f80bd60ca4eba40660f682eca1648ff994fdbd42
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-07-19 23:38:11 +0000

    Create a character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  // --- our code goes here ---
+  const maxLength = 140;
+  $('#tweet-text').keyup(function() {
+    const charLength = $(this).val().length;
+    const counter = maxLength - charLength
+    console.log("length", counter)
+    $('.counter').text(counter);
+    
+    if (counter < 0) {
+      $('.counter').css("color", "red")
+    } else {
+      $('.counter').css("color", "black")
+    }
+  })
+});
